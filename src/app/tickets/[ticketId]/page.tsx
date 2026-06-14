2ef00377e3a1a3e9202543efb4e82f7d979caaad
Usman Shahid <usmanshahiddev@gmail.com>
2026-06-14 18:19:03 +0100

    handle server side tickets fetching & added loading, error & suspense boundries

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,9 +1,6 @@
-import Link from "next/link";
-import { Placeholder } from "@/components/placeholder";
-import { Button } from "@/components/ui/button";
-import { initialTickets } from "@/data";
+import { notFound } from "next/navigation";
 import { TicketItem } from "@/features/ticket/components/ticket-item";
-import { ticketsPath } from "@/paths";
+import { getTicket } from "@/features/ticket/queries/get-ticket";
 
 type TicketPageProps = {
   params: Promise<{
@@ -13,20 +10,10 @@ type TicketPageProps = {
 
 const TicketPage = async ({ params }: TicketPageProps) => {
   const { ticketId } = await params;
-
-  const ticket = initialTickets.find((ticket) => ticket.id === ticketId);
+  const ticket = await getTicket(ticketId);
 
   if (!ticket) {
-    return (
-      <Placeholder
-        label="Ticket not found"
-        button={
-          <Button asChild variant="outline">
-            <Link href={ticketsPath()}>Go to Tickets</Link>
-          </Button>
-        }
-      />
-    );
+    return notFound();
   }
 
   return (
--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -1,12 +1,12 @@
 import { initialTickets } from "@/data";
 import { Ticket } from "../types";
 
-export const getTickets = async (): Promise<Ticket[]> => {
+export const getTicket = async (id: string): Promise<Ticket | null> => {
   await new Promise((resolve) => setTimeout(resolve, 2000));
 
-  // throw new Error("Failed to fetch tickets");
+  const maybeTicket = initialTickets.find((ticket) => ticket.id === id);
 
   return new Promise((resolve) => {
-    resolve(initialTickets);
+    resolve(maybeTicket || null);
   });
 };
